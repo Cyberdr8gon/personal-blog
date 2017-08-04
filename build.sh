1f00ab284740dbba11257ca5933f58960ce15229
Sam Bateman <sam@bateman.io>
2017-08-04 11:49:03 -0600

    auto-deploy

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 hugo -t coolnix
+git add .
+git status
+git commit -m "auto-deploy"
+git push
 cp -r public/* ../cyberdr8gon.github.io/
+pushd ../cyberdr8gon.github.io
+git add .
+git status
+git commit -m "auto-deploy"
+git push
